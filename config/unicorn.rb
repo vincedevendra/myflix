5805071c6a03cdeb85842ad5bd6fe9a3ff6bb1b2
Vincent J. DeVendra <VinceDeVendra@gmail.com>
2015-08-24 11:05:34 -0400

    Changed unicorn settings

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,8 +8,6 @@ before_fork do |server, worker|
     Process.kill 'QUIT', Process.pid
   end
 
-  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
-
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.connection.disconnect!
 end
@@ -21,4 +19,18 @@ after_fork do |server, worker|
 
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
+end
+
+before_fork do |server, worker|
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+end
+
+worker_processes 3
+after_fork do |server, worker|
+  Sidekiq.configure_client do |config|
+    config.redis = { :size => 1 }
+  end
+  Sidekiq.configure_server do |config|
+    config.redis = { :size => 5 }
+  end
 end
