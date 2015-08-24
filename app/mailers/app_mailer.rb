2a6b839f2923e1aa9090c87654ffe75a0cff994a
Vincent J. DeVendra <VinceDeVendra@gmail.com>
2015-08-24 19:32:48 -0400

    Send all email to me in staging environment

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,16 +1,21 @@
 class AppMailer < ActionMailer::Base
   def send_welcome_email(user)
     @user = user
-    mail from: 'info@myflix.com', to: user.email, subject: 'Welcome to MyFlix!'
+    mail from: 'info@myflix.com', to: recipient(user.email), subject: 'Welcome to MyFlix!'
   end
 
   def send_password_reset_email(user, token)
     @token = token
-    mail from: 'support@myflix.com', to: user.email, subject: "Instructions to Reset Your Password"
+    mail from: 'support@myflix.com', to: recipient(user.email), subject: "Instructions to Reset Your Password"
   end
 
   def send_invitation_email(invite, user)
     @invite = invite
-    mail from: 'info@myflix.com', to: invite.email, subject: "#{user.full_name} has invited you to try MyFlix!"
+    mail from: 'info@myflix.com', to: recipient(invite.email), subject: "#{user.full_name} has invited you to try MyFlix!"
   end
+
+  private
+    def recipient(address)
+      Rails.env == 'staging' ? 'vincedevendra@gmail.com' : address
+    end
 end
