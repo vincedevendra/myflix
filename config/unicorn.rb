618fdc16b25d1417aa75f7a5d5854f8267a9fda0
Vincent J. DeVendra <VinceDeVendra@gmail.com>
2015-08-24 15:35:09 -0400

    Removed sidekiq hack

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -20,17 +20,3 @@ after_fork do |server, worker|
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
 end
-
-before_fork do |server, worker|
-  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
-end
-
-worker_processes 3
-after_fork do |server, worker|
-  Sidekiq.configure_client do |config|
-    config.redis = { :size => 1 }
-  end
-  Sidekiq.configure_server do |config|
-    config.redis = { :size => 5 }
-  end
-end
